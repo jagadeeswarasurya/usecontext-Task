b5551602276c7a64e745826891ae84eb4baf7d47
jagadeeswarasurya <jagadeeswarasuryaj@gmail.com>
2024-09-09 12:14:06 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+React Shopping Cart Task Using useContext() This is a smple React application that demonstrates a basic shopping cart functionality.it includes a list of products with details such as title,description,price,discount,and stock.Users can view their selected products in a shopping cart and manage the quantity of each item. Topics Used: HTML CSS JAVASCRIPT BOOTSTRAP REACT
---
+++ src/Products.jsx
@@ -0,0 +1,69 @@
+
+export default [
+    {
+        "id": 1,
+        "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "price": 109.95,
+        "discountPercentage": 12.96,
+        "rating": 3.9,
+        "stock": 120,
+        "brand": "Fjallraven",
+        "category": "men's clothing",
+        "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+        "images": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+    },
+    {
+        "id": 2,
+        "title": "Mens Casual Premium Slim Fit T-Shirts",
+        "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+        "price": 22.3,
+        "discountPercentage": 17.94,
+        "rating": 4.1, //---------
+        "stock": 259,
+        "brand": "U.S POLO",
+        "category": "men's clothing",
+        "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+        "images": 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg'
+    },
+    {
+        "id": 3,
+        "title": "Mens Cotton Jacket",
+        "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+        "price": 55.99,
+        "discountPercentage": 15.46,
+        "rating": 4.7,
+        "stock": 500,
+        "brand": "U.S POLO",
+        "category": "men's clothing",
+        "thumbnail": "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+        "images": 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg'
+    },
+    {
+        "id": 4,
+        "title": "Mens Casual Slim Fit",
+        "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+        "price": 15.99,
+        "discountPercentage": 17.91,
+        "rating": 2.1,
+        "stock": 430,
+        "brand": "U.S POLO",
+        "category": "men's clothing",
+        "thumbnail": "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+        "images": 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg'
+    },
+    {
+        "id": 5,
+        "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+        "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+        "price": 695,
+        "discountPercentage": 10.58,
+        "rating": 4.6,
+        "stock": 400,
+        "brand": "Tanishq Bracelets",
+        "category": "jewelery",
+        "thumbnail": "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
+        "images": 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg'
+    }
+    
+]
